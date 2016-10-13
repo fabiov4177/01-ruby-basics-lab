b2c7587551f6396f5cdd6a1d45a0c244e3fef797
fabiov4177 <fabiov4177@Hstat.org>
2016-10-13 16:23:46 +0000

    Finish Homework

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,27 @@
-def division
+def division(num1,num2)
+return num1 / num2
     
 end
 
-def argue
-  
+def argue(arguement)
+return arguement
 end
 
-def greeting
+def greeting(hello, goodbye)
 
 end
 
 def return_a_value
+return "Nice"
     
 end
 
-def pizza_party
+def pizza_party(topping = "cheese")
+return topping
     
 end
 
-def whisper
+def whisper(quiet = whisper)
+return quiet.downcase
     
 end
